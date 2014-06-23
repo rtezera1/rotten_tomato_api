96cb080edeb11ad33f69bb217ef5b4a6162c6d5e
Robel <robeltadesse01@gmail.com>
2014-06-23 10:42:43 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenTomatoApi::Application.config.secret_key_base = 'c2d8530788946f263b1b8e2469185e2d578c6eb753aa47c38c45cb3f074248a5749e5cfbe587f6a4080346e68260e531c80e4025664adce7ee67ca4299e31afc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RottenTomatoApi::Application.config.session_store :cookie_store, key: '_rotten_tomato_api_session'
